0d41f95abb0977667cc30d5c5e19a58ad1ca2885
Marcin Kala <kalamarcin1@gmail.com>
2022-10-24 15:38:26 +0200

    final

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+	const [color, setColor] = useState('')
+	const [error, setError] = useState(false)
+	const [list, setList] = useState([])
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		let colors = new Values(color)
+	}
+	return (
+		<>
+			<section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={e => setColor(e.target.value)}
+          placeholder='#f15025'/>
+          
+          <button className='btn'>Submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        <h4>list goes here</h4>
+      </section>
+		</>
+	)
+}
+
+export default App
